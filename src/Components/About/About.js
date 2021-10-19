0411fd65c0cd879574c076567124dcb4b62b07b8
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-10-19 23:30:15 +0600

    afdfeg

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,11 +10,7 @@ const About = () => {
                 <h3 className="text-capitalize">we offer</h3>
                 <h2 className="text-capitalize"><strong>fast & reliable</strong></h2>
                 <h2 className="text-capitalize text-warning">Medical & healthcare solutions to our patients</h2>
-                <p>
-
-                    Such Bangladeshi workers / job seekers are “decked” or equally distributed among the Medical Centers accredited by Executive Board of the Health Ministers' Council for Gulf Cooperation Council (GCC) States.
-
-                    The medical centers providing these services in Dhaka are collectively known as the GCC Approved Medical Centers Association or GAMCA, Dhaka. It was organized in compliance with the GCC requirement that medical certificates issued by the GAMCA member centers only, will be honored by its embassies in the processing of employment papers.</p>
+                <p>The Health Care Agency is a regional provider, charged with protecting and promoting individual, family and community health through coordination of public and private sector resources. HCA's service environment is complex, with 180 different funding sources and over 200 State and Federal mandates. The mandates under which HCA operates require the County to provide for, or to regulate, certain health services. Many also carry specific requirements for staffing, operations, claiming and record-keeping.</p>
             </div>
         </div>
     );

79cb160999296a99885fd9ddbcf5290ccdd9645e
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-10-19 23:51:00 +0600

    final

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import image from './notfound.png'
 const NotFound = () => {
     return (
-        <div>
-            <h2>404</h2>
-            <h5>Page Not Found</h5>
+        <div className="mx-auto p-5 text-center">
+            <img className="w-75 text-center" src={image} alt="" srcset="" />
+            <h1 className="my-5">Please Go To Another Page</h1>
         </div>
     );
 };
